e739d7901c527f3e267b195601eac4e38d55096b
BrunoFelixDeSousa <brunofelixdesousa@hotmail.com>
2022-02-27 00:16:48 -0300

    add classe Cliente

--- src/Dio/bootcamp/oo/Cliente.java
+++ src/Dio/bootcamp/oo/Cliente.java
@@ -1,4 +1,15 @@
 package Dio.bootcamp.oo;
 
 public class Cliente {
+
+    private String nome;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
